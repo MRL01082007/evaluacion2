d436b8c64917dcf08f092ba1f1938699df24e9d7
eva2jorge <maximilianorugani@gmail.com>
2025-06-24 21:16:35 -0300

    views.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.evaluacion2, name=' evaluacion2'),
+    path('evaluacion2/', views.evaluacion2, name='evaluacion2'),
 ]

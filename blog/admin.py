0324545c10129a226893824ed794b32f324fb537
eva2jorge <maximilianorugani@gmail.com>
2025-06-26 19:40:13 -0300

    admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,26 +1,8 @@
 from django.contrib import admin
-from django.db import models
-from django.forms import DateInput
 from .models import Juegos_Indie
+from .forms import JuegosIndieForm
 
 class JuegosIndieAdmin(admin.ModelAdmin):
-    formfield_overrides = {
-        models.DateField: {
-            'widget': DateInput(
-                attrs={
-                    'type': 'date',
-                    'lang': 'es',
-                },
-                format='%Y-%m-%d',  
-            )
-        }
-    }
-
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        for field_name in ['Fecha_de_salida', 'Fecha_de_la_ultima_actualizacion']:
-            if field_name in form.base_fields:
-                form.base_fields[field_name].input_formats = ['%Y-%m-%d']
-        return form
+    form = JuegosIndieForm
 
 admin.site.register(Juegos_Indie, JuegosIndieAdmin)

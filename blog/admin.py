950c6b977adadcc3a4e93cbe6b05a1bebc70967c
eva2jorge <maximilianorugani@gmail.com>
2025-06-26 19:47:39 -0300

    admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Juegos_Indie
 from .forms import JuegosIndieForm
 
-class JuegosIndieAdmin(admin.ModelAdmin):
+#class JuegosIndieAdmin(admin.ModelAdmin):
     #form = JuegosIndieForm
 
 admin.site.register(Juegos_Indie)

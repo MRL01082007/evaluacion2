a30407f7061c4658582d37fa8d60421c6e3061bf
eva2jorge <maximilianorugani@gmail.com>
2025-06-26 19:10:51 -0300

    admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -3,8 +3,8 @@ from django.forms import DateInput
 from django.db import models
 from .models import Juegos_Indie
 
-class Juegos_Indie(admin.ModelAdmin):
+class JuegosIndieAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.DateField: {'widget': DateInput(attrs={'type': 'date'})},
     }
-admin.site.register(Juegos_Indie)
+admin.site.register(Juegos_Indie, JuegosIndieAdmin)

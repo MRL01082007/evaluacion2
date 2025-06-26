730e70d6f47c22142215e0f650c4a3a361c80638
eva2jorge <maximilianorugani@gmail.com>
2025-06-26 20:27:40 -0300

    admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 from .models import Juegos_Indie
-from .forms import JuegosIndieForm
 
-class JuegosIndieAdmin(admin.ModelAdmin):
-    form = JuegosIndieForm
-admin.site.register(Juegos_Indie, JuegosIndieAdmin)
+admin.site.register(Juegos_Indie)
--- blog/models.py
+++ blog/models.py
@@ -6,11 +6,11 @@ class Juegos_Indie(models.Model):
     autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200)
     Nombre_del_juego_indie = models.TextField()
-    Fecha_de_salida = models.DateField(blank=True, null=True)
+    Fecha_de_salida = models.DateTimeField(blank=True, null=True)
     Descripcion = models.TextField()
     Precio = models.TextField()
     Plataformas_disponibles = models.TextField()
-    Fecha_de_la_ultima_actualizacion = models.DateField(blank=True, null=True)
+    Fecha_de_la_ultima_actualizacion = models.DateTimeField(blank=True, null=True)
     fecha_publicacion = models.DateTimeField(default=timezone.now)
     hora_creacion = models.DateTimeField(blank=True, null=True)
 
